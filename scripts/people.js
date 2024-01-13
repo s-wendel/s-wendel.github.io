3940b4d6c83469eae1f01e3cbe4f340758dd0fc8
Shane Wendel <45246939+s-wendel@users.noreply.github.com>
2024-01-13 23:36:59 +0000

    Finished the visuals of the people

--- scripts/people.js
+++ scripts/people.js
@@ -1,4 +1,4 @@
-function addPerson() {
+function addPerson(name, description, image) {
 
     let people = document.getElementById("people");
 
@@ -6,13 +6,32 @@ function addPerson() {
     person.classList.add("person");
 
     let image = document.createElement("img");
-    image.src = "../assets/auzora_minions.gif";
+    image.src = "../assets/bedlessnoob.jpg";
+    image.style.borderRadius = "200px";
+
+    let header = document.createElement("h2");
+    header.innerHTML = name;
+    
+    let headerStyle = header.style;
+    headerStyle.marginLeft = "65%";
+    headerStyle.marginTop = "-50%";
+
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = description;
+    paragraph.style.marginLeft = "65%";
 
     person.appendChild(image, person.children[0]);
+    person.appendChild(header, person.children[0]);
+    person.appendChild(paragraph, person.children[0]);
 
     people.appendChild(person, people.children[0]);
 
 }
 
-addPerson();
-addPerson();
+
+async function addAllPeople() {
+
+    
+
+}
+
