362b8ce5176fb668728dc8f260b105173fcf3cd4
Shane Wendel <45246939+s-wendel@users.noreply.github.com>
2024-01-13 23:58:43 +0000

    Added Zackaria, clicking on a person opens their respective link

--- scripts/people.js
+++ scripts/people.js
@@ -1,30 +1,33 @@
-function addPerson(name, description, image) {
+function addPerson(person) {
 
     let people = document.getElementById("people");
 
-    let person = document.createElement("div");
-    person.classList.add("person");
+    let personElement = document.createElement("div");
+    personElement.classList.add("person");
+    personElement.addEventListener("click", function() {
+        onClick(person)
+    })
 
     let imageElement = document.createElement("img");
-    imageElement.src = "url(../assets/" + image + ")";
+    imageElement.src = "../assets/" + person.icon;
     imageElement.style.borderRadius = "200px";
 
     let header = document.createElement("h2");
-    header.innerHTML = name;
+    header.innerHTML = person.name;
     
     let headerStyle = header.style;
     headerStyle.marginLeft = "65%";
     headerStyle.marginTop = "-50%";
 
     let paragraph = document.createElement("p");
-    paragraph.innerHTML = description;
+    paragraph.innerHTML = person.description;
     paragraph.style.marginLeft = "65%";
 
-    person.appendChild(imageElement, person.children[0]);
-    person.appendChild(header, person.children[0]);
-    person.appendChild(paragraph, person.children[0]);
+    personElement.appendChild(imageElement, personElement.children[0]);
+    personElement.appendChild(header, personElement.children[0]);
+    personElement.appendChild(paragraph, personElement.children[0]);
 
-    people.appendChild(person, people.children[0]);
+    people.appendChild(personElement, people.children[0]);
 
 }
 
@@ -34,15 +37,19 @@ async function addAllPeople() {
     const jsonFetch = await fetch("https://raw.githubusercontent.com/s-wendel/s-wendel.github.io/main/assets/people.json");
     const json = await jsonFetch.json();
 
-    console.log(json);
-
     for(let i = 0; i < json.length; i++) {
 
         const person = json[i];
 
-        addPerson(person.name, person.description, person.icon);
+        addPerson(person);
 
     }
+
+}
+
+function onClick(person) {
+
+    window.open(person.link);
 
 }
 
